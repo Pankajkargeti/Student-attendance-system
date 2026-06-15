65d2c319193eb319355d0af795992a932a34bed0
ocegik <pankajkargeti1@gmail.com>
2026-06-15 21:00:00 +0530

    Set up React project structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+    return (
+        <main className="setup-message">
+            <h1>Student Attendance System</h1>
+            <p>React setup is ready for the next step.</p>
+        </main>
+    );
+}
+
+export default App;
